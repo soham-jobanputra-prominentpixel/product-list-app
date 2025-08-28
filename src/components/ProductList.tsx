466e76213fef1217e19dec3ecb48af530ea19ef4
Soham Jobanputra <soham.jobanputra@prominentpixel.com>
2025-08-28 18:38:18 +0530

    bugfix: Showing message when no matching product found - The filtering logic is moved inside ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,24 +1,49 @@
 import type React from "react";
 import Product from "./Product.tsx";
 import type ProductType from "../core/product.ts";
+import { useState } from "react";
+import SearchInput from "./SearchInput.tsx";
 
 type ProductListProps = {
-    products: ProductType[];
+    initialProducts: ProductType[];
 };
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
-    return (
-        <div className="flex flex-wrap gap-5">
-            {products.map(
-                (product) => (
-                    <Product
-                        key={product.id}
-                        name={product.name}
-                        price={product.price}
-                    />
+const ProductList: React.FC<ProductListProps> = ({ initialProducts }) => {
+    const [products, setProducts] = useState<ProductType[]>(initialProducts);
+
+    function filterProducts(searchText: string): void {
+        searchText = searchText.trim();
+        if (searchText.length === 0) {
+            setProducts(initialProducts);
+        } else {
+            setProducts(
+                initialProducts.filter((product) =>
+                    product.name.toLowerCase().includes(
+                        searchText.toLowerCase(),
+                    )
                 ),
-            )}
-        </div>
+            );
+        }
+    }
+
+    return (
+        <>
+            <SearchInput onSearch={filterProducts} />
+            <div className="mt-6"></div>
+            <div className="flex flex-wrap gap-5">
+                {products.length === 0
+                    ? "No products match your search. Please try different keywords or filters."
+                    : products.map(
+                        (product) => (
+                            <Product
+                                key={product.id}
+                                name={product.name}
+                                price={product.price}
+                            />
+                        ),
+                    )}
+            </div>
+        </>
     );
 };
 
